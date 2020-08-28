d981aa2e7d7050390ee69f1e0b92544e60430237
unknown <ms.ruchika23@gmail.com>
2020-08-29 01:46:29 +0530

    added 1 pgm

--- Codechef/DSA_Learning_Series/dsa3_standard_template_library/fence.cpp
+++ Codechef/DSA_Learning_Series/dsa3_standard_template_library/fence.cpp
@@ -5,7 +5,7 @@ https://www.codechef.com/LRNDSA03/problems/FENCE
 // RE for 70% of cases
 
 /*
-initialized f[n+2][m+2] = 0 to remove edge cases at border cells, count = 0
+initialized set of pairs of (row, col) for k plant cells, count = 0
 
 .   .   .   .   .   .
 .   .   .   .   x   .
@@ -14,10 +14,10 @@ initialized f[n+2][m+2] = 0 to remove edge cases at border cells, count = 0
 .   x   x   x   .   .
 .   .   .   .   .   .
 
-checked for 1 cell whether top, right, bottom cells were each without 1, incremented count
+checked if each top, right, bottom, left pairs were present in the set,
+if they were not present, incremented counter
 
 */
-
 
 #include <iostream>
 #include <bits/stdc++.h>
@@ -30,39 +30,26 @@ int main() {
 	while(T-- > 0) {
 	    long long int n, m, k, r, c, count = 0, i, j;
 	    cin >> n >> m >> k;
-	    long long int f[n+2][m+2];
-	    for(i = 0; i < n+2; i++) {
-	        for(j = 0; j < m+2; j++)
-	            f[i][j] = 0;
-	    }
+	    set<pair<long long int, long long int>> f;
 	    for(i = 0; i < k; i++) {
 	        cin >> r >> c;
-	        f[r][c] = 1;
+	        f.insert(make_pair(r,c));
 	    }
-	    /*for(long int i = 0; i < n+2; i++) {
-	        for(int j = 0; j < m+2; j++)
-	            cout << f[i][j] << " ";
-	        cout << endl;
-	    }
-	    cout << endl;*/
-	    for(i = 1; i < n+1; i++) {
-	        for(j = 1; j < m+1; j++) {
-	            if(f[i][j] == 1) {
-    	            // top
-    	            if(f[i-1][j] != 1)
-    	                count++;
-    	            // right
-    	            if(f[i][j+1] != 1)
-    	                count++;
-    	            // bottom
-    	            if(f[i+1][j] != 1)
-    	                count++;
-    	            // left
-    	            if(f[i][j-1] != 1)
-    	                count++;
-	            }
-	        }
-	           
+	    for(auto it : f) {
+	        r = it.first;
+	        c = it.second;
+	        // top
+	        if(f.find(make_pair(r-1, c)) == f.end())
+	            count++;
+	        // right
+	        if(f.find(make_pair(r, c-1)) == f.end())
+	            count++;
+	        // bottom
+            if(f.find(make_pair(r+1, c)) == f.end())
+	            count++;
+	        // left
+            if(f.find(make_pair(r, c-1)) == f.end())
+	            count++;          
 	    }
 	    cout << count << endl;
 	}
