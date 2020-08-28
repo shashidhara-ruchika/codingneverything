a48d453c34d5be2605ddb28abee3466644c110a5
unknown <ms.ruchika23@gmail.com>
2020-08-28 21:54:47 +0530

    added 1 pgm

---
+++ Codechef/DSA_Learning_Series/dsa3_standard_template_library/savkono.cpp
@@ -0,0 +1,47 @@
+/*
+https://www.codechef.com/LRNDSA03/problems/SAVKONO
+*/
+
+/*
+To find the min num of attacks, hits to Pain should be max
+So the soilder i with highest Ai should attack
+Ai can be the top of a priority queue, DS pq can store the Ai strengths
+*/
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while(T-- > 0) {
+	    long int n, z, ai, amax, count = 0;
+	    cin >> n >> z;
+	    priority_queue<long int> pq;
+	    for(int i = 0; i < n; i++) {
+            cin >> ai;
+            pq.push(ai);
+	    }
+	    while(pq.size() > 0) {
+	        count++;
+	        amax = pq.top();
+	        //cout << amax << " " << z << endl;
+	        pq.pop();
+	        z = z - amax;
+	        if(z <= 0) {
+	            break;
+	        }
+	        amax = floor(amax/2.0);
+	        if(amax > 0) {
+	            pq.push(amax);
+	        }
+	    }
+	    if(z <= 0)
+	        cout << count << endl;   
+        else
+            cout << "Evacuate\n";
+	}
+	return 0;
+}
