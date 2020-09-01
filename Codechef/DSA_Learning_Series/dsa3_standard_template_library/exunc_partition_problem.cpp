9061033f2816a5da04e58c8503d06a2f8fa9867e
unknown <ms.ruchika23@gmail.com>
2020-09-02 02:48:18 +0530

    partailly correct exunc

--- Codechef/DSA_Learning_Series/dsa3_standard_template_library/exunc_partition_problem.cpp
+++ Codechef/DSA_Learning_Series/dsa3_standard_template_library/exunc_partition_problem.cpp
@@ -2,7 +2,7 @@
 https://www.codechef.com/LRNDSA03/problems/EXUNC
 */
 
-// TLE
+// AC for 7 test cases, WA for 1 test case pts - 30/100
 
 /*
 types of q:
@@ -11,112 +11,68 @@ change a at index i
 update si at indexes i, i+1
 
 2 si
-print the first ele of si subarray i.e. lower bound of si
+print the first ele of si subarray i.e. upper bound of si
 */
 
 /*
 here indexing starts from 1, so it should be taken care
-store a & s= {0} as vectors
-whenver a new subarray is formed at index i, s[i] = 1, this happens during first ip of a & whenever query is 1
+store a vector & whenver new subarray is formed, store its index in s set
+this happens during first ip of a & whenever query is 1
 if query is 2, return the lower bound of si by counting the number os 1s till index i in si
 */
-
 #include <iostream>
 #include <bits/stdc++.h>
 
 using namespace std;
 
-//void printas(vector<long int> &a, vector<long int> &s);
-
 int main() {
 	long int n, q, ai, aend;
 	cin >> n >> q;
 	vector<long int> a;
-	vector<long int> s;
+	set<long int> s;
 	cin >> ai;
 	a.push_back(ai);
 	aend = ai;
-	s.push_back(1);
+	s.insert(1);
 	for(long int i = 2; i < n+1; i++) {
         cin >> ai;
-        if(ai % aend != 0) {
-            s.push_back(1);
-        }
-        else {
-            s.push_back(0);
-        }
+        if(ai % aend != 0)
+            s.insert(i);
         a.push_back(ai);
         aend = ai;
 	}
-	//printas(a, s);
-	//cout << endl;	    
-    long int qi, x, si;	    
-    for(long int qit = 0; qit < q; qit++) {
-        cin >> qi;
-        if(qi == 1) {
-            cin >> ai >> x;
-            //cout << ai << " " << x << endl;
-            auto abegin = a.begin();
-            auto sbegin = s.begin();
-            auto acurr = abegin + ai - 1;
-            auto scurr = sbegin + ai - 1;
-            *(acurr) = x;
-            if(ai == 1) {
-                *(scurr) = 1;
-                if(*(acurr + 1) % *acurr != 0) 
-                    *(scurr + 1) = 1;
-                else
-                    *(scurr + 1) = 0;
-            }
-            else if(ai == n) {
-                if(*(acurr) % *(acurr - 1) != 0)
-                    *(scurr) = 1;
-            }
-            else {
-                if(*(acurr) % *(acurr - 1) != 0 && *(acurr) % *(acurr + 1) == 0) {
-                    *(scurr) = 1;
-                    *(scurr + 1) = 0;
-                }
-                else if (*(acurr) % *(acurr - 1) == 0 && *(acurr) % *(acurr + 1) != 0) {
-                    *(scurr) = 0;
-                    *(scurr + 1) = 1;
-                }
-                else if (*(acurr) % *(acurr - 1) != 0 && *(acurr) % *(acurr + 1) != 0) {
-                    *(scurr) = 1;
-                    *(scurr + 1) = 1;
-                }
-                else {
-                    *(scurr) = 0;
-                    *(scurr + 1) = 0;
-                }
-            }
-            //printas(a, s);
-            //cout << endl;
-        }
-        if(qi == 2) {
-            cin >> si;    
-            //cout << si << endl;
-            long int sinew;
-            auto sbegin = s.begin();
-            for(long int sit = 0; sit < si; sit++) {
-                if(*(sbegin + sit) == 1)
-                    sinew = sit + 1;
-            }
-            cout << sinew << endl;
-        }
-    }
-    //cout << endl;	    
+	long int qi, x, si, oldai;
+	while(q--) {
+	    cin >> qi;
+	    if(qi == 1) {
+	        cin >> ai >> x;
+	        auto abegin = a.begin();
+	        auto ain = abegin + ai;
+	        auto ainbefore = ain - 1;
+	        auto ainafter = ain + 1;
+	        oldai = *ain;
+	        *ain = x;
+	        if(ai > 1) {
+	            if(x % *ainbefore != 0 )
+	                s.insert(ai);
+	            else if(x % *ainbefore == 0 )
+	                s.erase(ai);
+	        }
+	        if(ai < n) {
+	            if(*ainafter % x != 0 )
+	                s.insert(ai + 1);
+	            else if(*ainafter % x == 0)
+	                s.erase(ai + 1);
+	        }
+	    }
+	    if(qi == 2) {
+	        cin >> si;
+	        auto sit = s.upper_bound(si);
+	        sit--;
+	        cout << *sit << endl;
+	    }
+	}
 	return 0;
 }
 
-/*
-void printas(vector<long int> &a, vector<long int> &s) {
-    for(auto x: a)
-	    cout << x << " ";
-	cout << endl;
-	for(auto x: s)
-	    cout << x << " ";
-    cout << endl;
-}
-*/
 
