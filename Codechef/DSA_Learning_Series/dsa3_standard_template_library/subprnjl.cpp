a452c9a9f3991d7911a4e72818fe42b1d1aa2924
Ruchika Shashidhara <48530409+RuchikaShashidhara@users.noreply.github.com>
2020-09-03 03:03:37 +0530

    removed unwanted lines

--- Codechef/DSA_Learning_Series/dsa3_standard_template_library/subprnjl.cpp
+++ Codechef/DSA_Learning_Series/dsa3_standard_template_library/subprnjl.cpp
@@ -2,12 +2,10 @@
 https://www.codechef.com/LRNDSA03/problems/SUBPRNJL
 */
 
-#include <iostream>
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int main() {
-  
+int main() {  
   return 0;
 }  
