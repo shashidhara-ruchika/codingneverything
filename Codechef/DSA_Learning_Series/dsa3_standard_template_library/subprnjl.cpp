9fe26455b2208a6d99b9562bb04f194e84e0c9d5
unknown <ms.ruchika23@gmail.com>
2020-10-12 19:57:02 +0530

    added new solutions

--- Codechef/DSA_Learning_Series/dsa3_standard_template_library/subprnjl.cpp
+++ Codechef/DSA_Learning_Series/dsa3_standard_template_library/subprnjl.cpp
@@ -1,3 +1,10 @@
 /*
 https://www.codechef.com/LRNDSA03/problems/SUBPRNJL
 */
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+int main() {
+    return 0;
+}
---
+++ Leetcode/Easy/two_sum.cpp
@@ -0,0 +1,55 @@
+/*
+https://leetcode.com/problems/two-sum/
+*/
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        unordered_map<int, int> sum_pairs;
+        
+        for(int i = 0; i < nums.size(); i++) {
+            
+            if (sum_pairs.find(target - nums[i]) != sum_pairs.end())    
+                return {i, sum_pairs[target - nums[i]]};              
+                
+            else
+                sum_pairs[nums[i]] = i;  
+            
+        }
+        return {0, 0};
+    }  
+
+    void solve() {
+
+        int n, target, num;
+        vector<int> nums;        
+        
+        cin >> n;
+        for(int i = 0; i < n; i++) {
+            cin >> num;
+            nums.push_back(num);
+        }
+        cin >> target;
+
+        vector<int> sum_pair;
+
+        sum_pair = twoSum(nums, target);
+        cout << sum_pair[0] << " " << sum_pair[1] << endl;      
+        
+    }  
+        
+};
+
+int main() {
+
+    Solution mysoln;
+    mysoln.solve();
+    return 0;
+
+}
