aba0f3e1528e1f4dc9eca6f6d4f8551af115831d
unknown <ms.ruchika23@gmail.com>
2020-10-19 01:19:05 +0530

    added soln

---
+++ Leetcode/Easy/add_binary.cpp
@@ -0,0 +1,63 @@
+/*
+https://leetcode.com/problems/add-binary/
+*/
+
+# include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+    
+public:
+    string addBinary(string a, string b) {
+        
+        string result = "";
+        int sum = 0;
+        
+        int ai = a.length() - 1;
+        int bi = b.length() - 1;
+        
+        while(ai >= 0 || bi >= 0 || sum == 1) {
+            
+            if (ai >= 0)
+                sum += a[ai] - '0';
+            else
+                sum += 0;
+            if (bi >= 0)
+                sum += b[bi] - '0';
+            else
+                sum += 0;
+            
+            // append 1 if sum = 1 or 3, append 0 if sum = 0, 2 to result string
+            result = char( sum % 2 + '0') + result;
+            
+            // make carry as next sum
+            sum /= 2;
+            
+            ai--;
+            bi--;              
+        }
+        return result;        
+    }
+
+    void solve() {
+        string a, b, c;
+        cin >> a >> b;
+        c = addBinary(a, b);
+        cout << c << endl;
+    }
+};
+
+int main() {
+
+    /*
+    101 11
+    */
+
+    /*
+    1000
+    */
+   
+    Solution mysoln;
+    mysoln.solve();
+}
---
+++ Leetcode/Easy/sqrtx.cpp
@@ -0,0 +1,56 @@
+/*
+https://leetcode.com/problems/sqrtx/
+*/
+
+# include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int mySqrt(int x) {        
+        
+        if(x == 0 || x == 1)
+            return x;
+        
+        long long int l = 0, r = x/2, mid;
+        while (l < r) {
+            
+            mid = l + (r -l)/2;
+            
+            // mid squared is equal or mid + 1 squared just greater than x
+            if (mid * mid <= x && (mid+1) * (mid + 1) > x)
+                return mid;
+            // if mid sqaured is greater than x, search in smaller left array
+            if (mid * mid > x)    
+                 r = mid - 1;
+            // if mid squared is smaller than x, search in larger right array
+            else
+                l = mid + 1;                
+            
+        }
+        return l;               
+    }
+
+    void solve() {
+        int x;
+        cin >> x;
+        cout << mySqrt(x) << endl;
+    }
+};
+
+int main() {
+
+    /*
+    8
+    */
+
+    /*
+    2
+    */
+    
+    Solution mysoln;
+    mysoln.solve();
+
+    return 0;
+}
