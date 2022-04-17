bce922182e6201ad67c89671e9d1fabafa79623a
Ruchika Shashidhara <48530409+RuchikaShashidhara@users.noreply.github.com>
2022-04-17 22:02:44 +0530

    added files

---
+++ Data-Structures-Algorithms/Sorting/java-comparator-sort.java
@@ -0,0 +1,71 @@
+/*
+https://www.hackerrank.com/challenges/java-comparator/problem?isFullScreen=true
+*/
+
+import java.util.*;
+
+// Write your Checker class here
+
+class Checker implements Comparator<Player>{
+
+    @Override
+    public int compare(Player o1, Player o2) {
+        if (o1.score == o2.score) {
+            return o1.name.compareTo(o2.name);
+        }
+        else {
+            return o2.score - o1.score;
+        }
+    }
+    
+}
+
+class Player{
+    String name;
+    int score;
+    
+    Player(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+}
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+        
+        for(int i = 0; i < n; i++){
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+     
+        Arrays.sort(player, checker);
+        for(int i = 0; i < player.length; i++){
+            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        }
+    }
+}
+
+/*
+Sample Input
+
+5
+amy 100
+david 100
+heraldo 50
+aakansha 75
+aleksa 150
+
+Sample Output
+
+aleksa 150
+amy 100
+david 100
+aakansha 75
+heraldo 50
+*/
