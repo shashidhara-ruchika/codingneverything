c0cea6b447204bb2cc58785db8004a89b8020d0e
Ruchika Shashidhara <48530409+RuchikaShashidhara@users.noreply.github.com>
2022-04-18 01:02:26 +0530

    added files

---
+++ Object-Oriented-Programming/Java/Design-Patterns/factory-pattern.java
@@ -0,0 +1,103 @@
+/*
+https://www.hackerrank.com/challenges/java-factory/problem?isFullScreen=true
+*/
+
+import java.util.*;
+import java.security.*;
+interface Food {
+	 public String getType();
+	}
+	class Pizza implements Food {
+	 public String getType() {
+	 return "Someone ordered a Fast Food!";
+	 }
+	}
+
+	class Cake implements Food {
+
+	 public String getType() {
+	 return "Someone ordered a Dessert!";
+	 }
+	}
+	class FoodFactory {
+		public Food getFood(String order) {
+
+//Write your code here
+
+            switch(order) {
+                case "cake":
+                    return new Cake();
+                case "pizza":
+                    return new Pizza();
+                default:
+                    return null;
+            }
+
+}//End of getFood method
+
+	}//End of factory class
+
+	public class Solution {
+
+	 public static void main(String args[]){
+			Do_Not_Terminate.forbidExit();
+
+		try{
+
+			Scanner sc=new Scanner(System.in);
+			//creating the factory
+			FoodFactory foodFactory = new FoodFactory();
+	
+			//factory instantiates an object
+			Food food = foodFactory.getFood(sc.nextLine());
+	
+			
+			System.out.println("The factory returned "+food.getClass());
+			System.out.println(food.getType());
+		}
+		catch (Do_Not_Terminate.ExitTrappedException e) {
+			System.out.println("Unsuccessful Termination!!");
+		}
+	 }
+
+	}
+	class Do_Not_Terminate {
+		 
+	    public static class ExitTrappedException extends SecurityException {
+
+			private static final long serialVersionUID = 1L;
+	    }
+	 
+	    public static void forbidExit() {
+	        final SecurityManager securityManager = new SecurityManager() {
+	            @Override
+	            public void checkPermission(Permission permission) {
+	                if (permission.getName().contains("exitVM")) {
+	                    throw new ExitTrappedException();
+	                }
+	            }
+	        };
+	        System.setSecurityManager(securityManager);
+	    }
+	}	
+		
+/*
+Sample Input 1
+
+cake
+
+Sample Output 1
+
+The factory returned class Cake
+Someone ordered a Dessert!
+
+Sample Input 2
+
+pizza
+
+Sample Output 2
+
+The factory returned class Pizza
+Someone ordered Fast Food!
+*/
+
