1adc7abe01526890bce729f25eab637b55d1fb92
shashidhara-ruchika <shashidhara.r@northeastern.edu>
2023-11-06 15:28:41 -0500

    2

---
+++ Data-Structures-Algorithms/Linked-Lists/Singly-Linked-Lists/linked-list-cycle.java
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/linked-list-cycle/description/?envType=study-plan-v2&envId=top-interview-150
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
