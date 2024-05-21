108de2ec06ff96f79af669b6dc27581dc72810d4
shashidhara-ruchika <shashidhara.r@northeastern.edu>
2024-05-20 21:12:36 -0400

    3

---
+++ Data-Structures-Algorithms/Trees/Binary-Trees/balanced-binary-tree.java
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/balanced-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    boolean isbal;
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        isbal = true;
+
+        getHeight(root);
+
+        return isbal;
+    }
+
+    public int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            isbal = false;
+        }
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    
+}
---
+++ Data-Structures-Algorithms/Trees/Binary-Trees/diameter-of-a-binary-tree.java
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/diameter-of-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int diameter = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        maxDepth(root);
+        return diameter;
+    }
+
+    int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int l = maxDepth(root.left);
+        int r = maxDepth(root.right);
+
+        diameter = Math.max(diameter, l + r);
+        return 1 + Math.max(l, r);
+
+    }
+}
